74ddfeb851c504409ab2dea346d5654743cfc223
berkayalp <ufenerbahce@hotmail.com>
2018-09-09 21:43:44 +0300

    feat: add book component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,11 +9,15 @@ import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app-routing.module';
 import { UserService } from './domain/service';
 import { BookService } from './domain/service';
+import { BookComponent } from './book/book.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    BookComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
